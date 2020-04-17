7483ec614e76478016075db3e585df3da85879b3
yamineben <yamine.benmessaoud@utbm.fr>
2020-04-17 16:52:35 +0200

    Création

---
+++ cypress/integration/conduit.spec.js
@@ -0,0 +1,22 @@
+describe('the conduit application', () => {
+  it('shows some posts', () => {
+    cy.visit('/')
+    cy.get('.article-preview').should('have.length', 10)
+  })
+
+ /* it('shows the first post', () => {
+      cy.server()
+      // we set the response to be the activites.json fixture
+      cy.route('GET', '/api/articles*', 'fixture:posts.json')
+      cy.visit('/')
+      cy.get(':nth-child(1) > .article-preview').contains('test-moh')
+  })*/
+
+  it('should handle an empty database', () => {
+      cy.server()
+      // we set the response to be the activites.json fixture
+      cy.route('GET', '/api/articles*', 'fixture:no_posts.json')
+      cy.visit('/')
+      cy.contains('No articles are here... yet.')
+    })
+})
